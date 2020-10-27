6b850c91fb5c25faf4248e8daa90d97bb187e9fd
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-27 17:14:22 -0600

    Add test for incomplete user form

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'On the registration page' do
-  it 'it has a form' do
+  it 'it creates a new user when the form is filled out completely' do
     visit '/'
 
     click_on 'Register User'
@@ -21,5 +21,27 @@ RSpec.describe 'On the registration page' do
 
     expect(page).to have_content('You are now registered and logged in!')
     expect(page).to have_content('Hello, Grant!')
+  end
+
+    it 'it shows a flash message when the form is incomplete' do
+    visit '/'
+
+    click_on 'Register User'
+    expect(current_path).to eq('/register/new')
+
+    fill_in :name, with: 'Grant'
+    fill_in :address, with: '123 Grant Ave.'
+    fill_in :city, with: 'Denver'
+    fill_in :state, with: 'CO'
+    fill_in :zip, with: 80015
+    fill_in :email, with: ''
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
+    click_on 'Submit'
+
+    expect(current_path).to eq('/register/new')
+
+    expect(page).to have_content('Please fill in all required fields.')
+    expect(page).to_not have_content('You are now registered and logged in!')
   end
 end
