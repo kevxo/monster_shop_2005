681f76293491effb72704b26c9e4b397f5cf9eaf
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-27 16:52:31 -0600

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
@@ -33,4 +33,8 @@ Rails.application.routes.draw do
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
   get "/orders/:id", to: "orders#show"
+
+  get '/register/new', to: 'users#new'
+  post '/users', to: 'users#create'
+  get '/profile', to: 'users#show'
 end
