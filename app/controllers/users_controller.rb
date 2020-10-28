8703194b41ae38a995b4edfe2e56486a196df7d0
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-28 17:12:11 -0600

    Refactor create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    if !User.find_by(email: params[:email])
-      @new_user = User.create(user_params)
-      if @new_user.save
-        flash[:success] = "You are now registered and logged in!"
-        session[:user_id] = @new_user.id
-        redirect_to "/profile"
-      else
-        flash[:error] = "Please fill in all required fields."
-        redirect_to '/register/new'
-      end
+    @new_user = User.create(user_params)
+    if @new_user.save
+      flash[:success] = "You are now registered and logged in!"
+      session[:user_id] = @new_user.id
+      redirect_to "/profile"
     else
-      flash[:error]= 'Sorry, email already in use. Please enter a valid email address.'
+      flash[:error] = @new_user.errors.full_messages
       redirect_to '/register/new'
     end
   end
