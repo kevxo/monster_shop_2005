d1691699b0019dbb9103a744fd0157ddccdcc8b4
Carson Jardine <carsonjardine@gmail.com>
2020-10-28 11:48:33 -0600

    Update code for US3 user nav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < ApplicationController
   def new
-    
+
   end
 
+  def create
+    @user = User.find_by(email: params[:email])
+    session[:user_id] = @user.id
+    redirect_to '/profile'
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -41,4 +41,6 @@ Rails.application.routes.draw do
 
   #sessions
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
