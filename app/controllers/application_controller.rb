1589de7d4e3c127addd48afa86ddb2251657e3a9
Grant Dempsey <CryptoDemps@gmail.com>
2020-10-28 21:44:32 -0600

    alignment adjustment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def current_merchant?
@@ -18,5 +18,9 @@ class ApplicationController < ActionController::Base
 
   def current_admin?
     current_user && current_user.admin?
+  end
+
+  def current_default?
+    current_user && current_user.default?
   end
 end
