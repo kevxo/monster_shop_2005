9a1cf0bf3d383a4a2ea65d1a36a234515006a807
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-01 11:46:25 -0700

    Fix frozen string literal error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,6 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# require 'support/factory_bot'
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
