c411e64bb30d962851c33ca44ace3d0b98c6ec53
Grant Dempsey <CryptoDemps@gmail.com>
2020-10-31 22:11:56 -0600

    Add log in to test setup so checkout button is visible.

--- spec/features/orders/new_spec.rb
+++ spec/features/orders/new_spec.rb
@@ -115,4 +115,58 @@ RSpec.describe("New Order Page") do
     expect(page).to have_link("register")
     expect(page).to have_link("log in")
   end
+
+  it "When attempting to checkout from cart with items in cart I see info telling me I must register or log in with links" do
+    mike = Merchant.create!(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    tire = meg.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    paper = mike.items.create!(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+    pencil = mike.items.create!(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+    visit "/items/#{paper.id}"
+    click_on "Add To Cart"
+    visit "/items/#{paper.id}"
+    click_on "Add To Cart"
+    visit "/items/#{tire.id}"
+    click_on "Add To Cart"
+    visit "/items/#{pencil.id}"
+    click_on "Add To Cart"
+
+    visit "/cart"
+
+    click_on "log in"
+    user_1 = User.create!(name: 'Grant',
+                          address: '124 Grant Ave.',
+                          city: 'Denver',
+                          state: 'CO',
+                          zip: 12_345,
+                          email: 'grant@coolguy.com',
+                          password: 'password',
+                          role: 0)
+    visit '/login'
+
+    fill_in :email, with: user_1.email
+    fill_in :password, with: user_1.password
+
+    click_on 'Submit'
+
+    expect(current_path).to eq("/profile")
+    click_on "Cart"
+    expect(current_path).to eq("/cart")
+
+    click_on "Checkout"
+
+    expect(current_path).to eq("/orders/new")
+
+    fill_in :name, with: user_1.name
+    fill_in :address, with: user_1.address
+    fill_in :city, with: user_1.city
+    fill_in :state, with: user_1.state
+    fill_in :zip, with: user_1.zip
+    click_on "Create Order"
+
+    expect(current_path).to eq('/profile/orders')
+    expect(page).to have_content("Order successfully created!")
+    expect(page).to have_content("Cart: 0")
+  end
 end
