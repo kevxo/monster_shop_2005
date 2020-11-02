0aa92ca271bd8fa5ea973345c3fe01d30097dfdb
Carson Jardine <carsonjardine@gmail.com>
2020-11-01 19:34:19 -0700

    Update code to pass US30

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -1,14 +1,24 @@
 class UserOrdersController < ApplicationController
   def index
-    flash[:notice] = "Order successfully created!"
-
     @user = User.find(session[:user_id])
     @orders = @user.orders
+
+    flash[:notice] = "Order successfully created!"
   end
 
   def show
     @user = User.find(session[:user_id])
     @order = @user.orders.find(params[:order_id])
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+    @order = @user.orders.find(params[:order_id])
+    # @order.return_quantity
+
+    @order.update(status: "Cancelled")
+    redirect_to "/profile"
+    flash[:notice] = "You have successfully cancelled your order"
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip
+  validates_presence_of :name, :address, :city, :state, :zip, :status
   validates :user_id, presence: true, allow_nil: true
 
   has_many :item_orders
@@ -10,11 +10,30 @@ class Order < ApplicationRecord
     item_orders.sum('price * quantity')
   end
 
-  def default_status
-    self.status ||= "pending"
+  def total_quantity
+    if self.status == "Cancelled"
+       0
+    else
+      item_orders.sum(:quantity)
+    end
   end
 
-  def total_quantity
-    item_orders.sum(:quantity)
+  def remove_quantity
+    #ASK ALEX 
+    items.each do |item|
+      remove = ItemOrder.find_by(item_id: item.id)
+      item.inventory -= remove.quantity
+      item.save
+    end
   end
+  #returns quantity to merchant
+  def return_quantity
+    items.each do |item|
+      returned = ItemOrder.find_by(item_id: item.id)
+      item.inventory += returned.quantity
+      item.save
+    end
+  end
+
+
 end
