be5fa77aff859f9513042e56ef36edd20ec1d730
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-03 16:50:52 -0700

    Add view and controller for inactivate item

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -6,8 +6,13 @@ class Merchant::ItemsController < Merchant::BaseController
   def update
     @merchant = Merchant.find_by(id: current_user.merchant_id)
     item = Item.find(params[:item_id])
-    item.update(activation_status: 'Deactivated')
-    flash[:success] = "#{item.name} was deactivated."
-    redirect_to '/merchant/items'
+    if item.activation_status == 'Deactivated'
+      item.update(activation_status: 'Activated')
+      flash[:success] = "#{item.name} was activated."
+    else
+      item.update(activation_status: 'Deactivated')
+      flash[:success] = "#{item.name} was deactivated."
+    end 
+      redirect_to '/merchant/items'
   end
 end
