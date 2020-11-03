790e10167b9b133b325d2b5c7e98ab83fb3e6739
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-03 16:19:19 -0700

    Change active? to activation_status

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -6,7 +6,7 @@ class Merchant::ItemsController < Merchant::BaseController
   def update
     merchant = Merchant.find_by(id: current_user.merchant_id)
     item = Item.find(params[:item_id])
-    item.update(activation_status: 'deactivated')
+    item.update(activation_status: 'Deactivated')
     redirect_to '/merchant/items'
     flash[:success] = "Item was deactivated."
   end
--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Merchant Items Index Page" do
       @merchant_1 = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @merchant_1.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @chain = @merchant_1.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @merchant_1.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      @shifter = @merchant_1.items.create(name: "Shimano Shifters", description: "It'll always shift!", activation_status: 'Deactivated', price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
       @user_1 = @merchant_1.users.create!(name: 'Grant', address: '124 Grant Ave.', city: 'Denver', state: 'CO', zip: 12345, email: 'grant@coolguy.com', password: 'password', role: 1)
       
       visit '/login'
