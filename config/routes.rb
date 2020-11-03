1f0ea2364c54e308c4dc1aa76be47da257ce7c4d
Grant Dempsey <CryptoDemps@gmail.com>
2020-11-02 17:23:33 -0700

    Add admin namespace route for merchant index

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/', to: 'dashboard#index'
+    get '/merchants/:id', to: 'dashboard#index'
   end
 
   get '/profile', to: 'users#show'
