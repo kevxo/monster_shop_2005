3127837c0fadb61d1a2088a00ea83d1ac2be1c7e
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-05 16:33:02 -0700

    Update test validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of :address }
@@ -10,6 +9,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
     it { should validate_presence_of :email }
+    it { should validate_presence_of :name }
   end
 
   describe 'relationships' do
