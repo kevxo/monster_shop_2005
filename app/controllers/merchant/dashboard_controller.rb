2792cb313fcf5d2c62c47f68a5d83b84a8558760
Kevin Cuadros <kev040798@gmail.com>
2020-11-05 17:47:19 -0500

    Add @order to the controller

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Merchant::DashboardController < Merchant::BaseController
   def index
     flash[:success] = 'Logged In!'
     @merchant = Merchant.find_by(id: current_user.merchant_id)
+    @order =  Order.find(@merchant.item_orders.pluck(:order_id))
   end
 end
