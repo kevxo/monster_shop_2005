8d47ad69d7a5e088a3e495d78d68aca83d355806
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-05 18:38:06 -0700

    Change package order method

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -5,11 +5,10 @@ class Merchant::OrdersController < Merchant::BaseController
   end
 
   def update
-    @order = Order.find(params[:order_id])
-    @item_order = ItemOrder.find(params[:id])
+    @item_order = ItemOrder.find(params[:order_id])
     @item_order.item.update(inventory: (@item_order.item.inventory - @item_order.quantity))
     @item_order.update(fulfilled: true)
-    @order.change_status
+    @item_order.order.package_order
     flash[:notice] = "#{@item_order.id} has been fulfilled."
     redirect_to "/merchant/orders/#{@item_order.order_id}"
   end
