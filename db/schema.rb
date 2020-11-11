53da1c13457cb5254a1c1c6988457e03daf24487
Grant Dempsey <CryptoDemps@gmail.com>
2020-11-10 21:12:04 -0700

    Version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_030140) do
+ActiveRecord::Schema.define(version: 2020_11_11_035617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_11_11_030140) do
     t.integer "percent"
     t.integer "item_quantity"
     t.bigint "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["merchant_id"], name: "index_discounts_on_merchant_id"
   end
 
