15c910aea94df74031a251ca0f7aa4cf52965a8c
Grant Dempsey <CryptoDemps@gmail.com>
2020-11-10 21:09:35 -0700

    Build out discounts controller with index and show

---
+++ app/controllers/merchant/discounts_controller.rb
@@ -0,0 +1,15 @@
+class Merchant::DiscountsController < Merchant::BaseController
+  def index
+    @merchant = current_user.merchant
+  end
+
+  def show
+    @discount = Discount.find(params[:id])
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:percent, :item_quantity, :merchant_id)
+  end
+end
